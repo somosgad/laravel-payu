594d8a47785a59343efd0f0360ffa27d8bc9586f
Giovanni Pires da Silva <giovanni@somosgad.com>
2019-12-11 12:02:29 -0300

    Start create payment request

--- src/LaravelPayU.php
+++ src/LaravelPayU.php
@@ -2,10 +2,27 @@
 
 namespace SomosGAD_\LaravelPayU;
 
+use GuzzleHttp\Client;
+
 class LaravelPayU
 {
     public static function createPayment() {
+        $http = new Client;
         $url = 'https://api.paymentsos.com/payments';
-        return $url;
+        $form_params = [
+            'amount' => 2000,
+            'currency' => 'USD',
+        ];
+        $headers = [
+            'app-id' => env('PAYU_APP_ID'),
+            'private_key' => env('PAYU_PRIVATE_KEY'),
+            'Content-Type' => 'application/json',
+            'api-version' => '1.3.0',
+            'x-payments-os-env' => env('PAYU_ENV'),
+            'idempotency_key' => 'cust-34532-trans-001356-p',
+        ];
+        $response = $http->post($url, compact('form_params', 'headers'));
+
+        return json_decode((string) $response->getBody(), true);
     }
 }
