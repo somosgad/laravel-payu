daa8921ba96473ca1e12b70356351fa2b8f5b7e0
Camilo Cunha de Azevedo <camilotk@gmail.com>
2020-01-21 18:28:56 -0300

    Added test to the modified payment method for cash transactions

--- tests/LaravelPayUTest.php
+++ tests/LaravelPayUTest.php
@@ -148,7 +148,7 @@ class LaravelPayUTest extends TestCase
      * @depends SomosGAD_\LaravelPayU\Tests\InstanceTest::testInstance
      * @return array
      */
-    public function testCreateCustomer(LaravelPayU $payu)
+    public function testCreanteCustomer(LaravelPayU $payu)
     {
         $customer_reference = 'johntravolta18021954';
         $email = 'john@travolta.com';
@@ -266,6 +266,45 @@ class LaravelPayUTest extends TestCase
         $amount = 2000;
         $currency = 'USD';
         $payment = $payu->createPayment($amount, $currency);
+
+        $this->assertIsArray($payment);
+
+        $this->assertArrayHasKey('id', $payment);
+        $this->assertArrayHasKey('currency', $payment);
+        $this->assertArrayHasKey('created', $payment);
+        $this->assertArrayHasKey('modified', $payment);
+        $this->assertArrayHasKey('status', $payment);
+        $this->assertArrayHasKey('possible_next_actions', $payment);
+        $this->assertArrayHasKey('amount', $payment);
+
+        $this->assertIsString($payment['id']);
+        $this->assertIsString($payment['currency']);
+        $this->assertIsNumeric($payment['created']);
+        $this->assertIsNumeric($payment['modified']);
+        $this->assertIsString($payment['status']);
+        $this->assertIsArray($payment['possible_next_actions']);
+        $this->assertIsNumeric($payment['amount']);
+
+        return $payment;
+    }
+
+    /**
+     * Test create cash payment.
+     *
+     * @depends SomosGAD_\LaravelPayU\Tests\InstanceTest::testInstance
+     * @return void
+     */
+    public function testCreateCashPayment(LaravelPayU $payu)
+    {
+        $amount = 2000;
+        $currency = 'USD';
+        $payment = $payu->createPayment(
+            $amount,
+            $currency,
+            true,
+            null,
+            "Payment test for www.somosgad.com"
+        );
 
         $this->assertIsArray($payment);
 
