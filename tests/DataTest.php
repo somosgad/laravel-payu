2c82d67e44713991b55b6cae46f875778e39f491
Giovanni Pires da Silva <giovanni@somosgad.com>
2020-01-24 12:28:53 -0300

    Data test

---
+++ tests/DataTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace SomosGAD_\LaravelPayU\Tests;
+
+class DataTest extends TestCase
+{
+    /**
+     * @dataProvider additionProvider
+     */
+    public function testAdd($a, $b, $expected)
+    {
+        $this->assertSame($expected, $a + $b);
+    }
+
+    public function additionProvider()
+    {
+        return [
+            [0, 0, 0],
+            [0, 1, 1],
+            [1, 0, 1],
+            [1, 1, 3]
+        ];
+    }
+}
